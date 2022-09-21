92c7e4f252a1cf6c0dd8fd51c7d4fd676a8406a2
Sara <saralins19@gmail.com>
2022-09-21 20:45:14 -0300

    Projeto dia 1: ok

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 import express from "express";
-import "dotenv/config"
+import "dotenv/config";
 
 const app = express();
 app.use(express.json());
-    
-const PORT = process.env.PORT || 3000   
 
-app.listen(PORT, () => {
-    console.log(`server is running at port ${PORT}`)
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello Word");
 });
 
-export default app
+app.listen(PORT, () => {
+  console.log(`server is running at port ${PORT}`);
+});
+
+export default app;
